9b6a99d55cfd47c0184578fd7350547378c51f6c
Biwash Budhathoki <075bct025@ioepc.edu.np>
2023-12-28 13:18:52 +0545

    First stage of trivia quiz app completed

--- src/Components/Quiz/Quiz1.jsx
+++ src/Components/Quiz/Quiz1.jsx
@@ -1,48 +1,134 @@
 import React, { useState, useEffect } from 'react';
+import './Quiz.css';
 
 const Quiz1 = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [index, setIndex] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [lock, setLock] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const [result, setResult] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://opentdb.com/api.php?amount=10&category=9');
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+    
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://opentdb.com/api.php?amount=10&category=9');
-        const result = await response.json();
-        console.log(result);
-        console.log("Type of a question is :" + result.results[index + 1].type);
-        console.log("Question is:" + result.results[index + 1].question);
-        setData(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchData();
-  }, [index]); // Include index as a dependency if you use it in useEffect
+  }, []);
 
-  // Check if data is null before attempting to map over it
-  if (data === null) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    if (data && data.results) {
+      const currentQuestion = data.results[currentQuestionIndex];
+      const allOptions = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer];
+      shuffleArray(allOptions);
+      setOptions(allOptions);
+      setCorrectAnswer(currentQuestion.correct_answer);
+    }
+  }, [data, currentQuestionIndex]);
 
-  // Check if data.results is an array before attempting to map over it
-  if (!Array.isArray(data.results)) {
-    return <div>Error: Invalid data format</div>;
-  }
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+  const handleNextQuestion = () => {
+    if (lock === true) {
+      // Remove styling from all list items
+      const options = document.querySelectorAll('ul li');
+      options.forEach((option) => {
+        option.classList.remove('correct', 'wrong');
+      });
+  
+      if (currentQuestionIndex < (data?.results?.length || 0) - 1) {
+        setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+      } else {
+        // Quiz completed, handle it as needed
+        setResult(true);
+      }
+  
+      // Reset state for the next question
+      setLock(false);
+      setCorrectAnswer(null);
+      setOptions([]);
+    }
+  };
+
+  const checkAnswer = (e, ans) => {
+    if (!lock) {
+      if (ans === correctAnswer) {
+        // Selected answer is correct
+        e.target.classList.add("correct");
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        // Selected answer is wrong
+        e.target.classList.add("wrong");
+
+        // Find the element with the correct answer and add the "correct" class
+        const options = document.querySelectorAll('ul li');
+        options.forEach((option) => {
+          if (option.innerText === correctAnswer) {
+            option.classList.add("correct");
+          }
+        });
+      }
+
+      setLock(true);
+    }
+  };
+
+  const handleReset = () => {
+    // Reset all state variables to their initial values
+    setData(null);
+    setLoading(false);
+    setCurrentQuestionIndex(0);
+    setLock(false);
+    setOptions([]);
+    setCorrectAnswer(null);
+    setScore(0);
+    setResult(false);
+
+    // Fetch new data to start the quiz again
+    fetchData();
+  };
 
   return (
-    <div>
-      <h1>Data from API</h1>
-      {/* Render the data in your JSX */}
-      <ul>
-        {data.results.map((item) => (
-          <li key={item.question}>{item.question}</li>
-        ))}
-      </ul>
+    <div className='container'>
+      <h1>Trivia Quiz App</h1>
+      <hr />
+    
+      {result ? (
+        <div>
+          <h3>Quiz Completed</h3>
+          <p>Your Score: {score}</p>
+          <button onClick={handleReset}>Reset Quiz</button>
+        </div>
+      ) : (
+        <div>
+        <h2>{currentQuestionIndex+1}. {data?.results?.[currentQuestionIndex]?.question || ''}</h2>
+        <ul>
+          {options.map((option, optionIndex) => (
+            <li key={optionIndex} onClick={(e) => checkAnswer(e, option)}>
+              {option}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleNextQuestion}>Next Question</button>
+      </div>
+      )}
     </div>
   );
 };
